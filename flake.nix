068b92340d9edf1fc60d159d9152fd265b47e22e
anirrudh <anik597@gmail.com>
2023-06-23 11:14:32 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -4,27 +4,40 @@
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs =
-    { self
-    , flake-utils
-    , devshell
-    , nixpkgs
-    ,
-    }:
+  outputs = {
+    self,
+    flake-utils,
+    devshell,
+    nixpkgs,
+  }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = { allowUnfree = true; };
-            overlays = [ devshell.overlays.default ];
-          };
-        in
+      devShell = let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+          overlays = [devshell.overlays.default];
+        };
+      in
         pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-          devshell.packages = with pkgs; [
-            (python3.withPackages (p: [ p.numpy p.pydantic p.torch p.rich p.ipython p.psutil p.bitsandbytes p.accelerate ]))
-          ];
+          imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+
+          devshell.packages =
+            if pkgs.system == "*-linux"
+            then
+              with pkgs; [
+                openai-whisper-cpp
+                (python3.withPackages (p: [p.numpy p.pydantic p.torch p.rich p.ipython p.psutil p.bitsandbytes p.accelerate]))
+              ]
+            else if pkgs.system == "*-darwin"
+            then
+              with pkgs; [
+                openai-whisper-cpp
+                (python3.withPackages (p: [p.numpy p.pydantic p.torch p.rich p.ipython p.psutil]))
+              ]
+            else
+              with pkgs; [
+                (python3.withPackages (p: [p.numpy p.pydantic p.torch p.rich p.ipython p.psutil]))
+              ];
         };
     });
 }
